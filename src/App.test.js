2ccdf71d97ffd0b2d7f02a5754dc8b37315acb26
Kevin Raoofi <nullindev@gmail.com>
2017-11-09 11:10:20 -0500

    Made App test use the MuiThemeProvider

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<MuiThemeProvider><App /></MuiThemeProvider>, div);
 });
