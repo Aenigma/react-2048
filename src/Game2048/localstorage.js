d3f124e936fc5a1947aac112d02f5d8f124c87ad
Kevin Raoofi <nullindev@gmail.com>
2017-11-10 14:25:12 -0500

    added versioning to the localStorage

--- src/Game2048/localstorage.js
+++ src/Game2048/localstorage.js
@@ -1,8 +1,11 @@
+const version = "1.0.0";
+
 export const loadState = () => {
   try {
+    const stateVersion = localStorage.getItem('state-version');
     const serializedState = localStorage.getItem('state');
 
-    if (serializedState === null) {
+    if (serializedState === null || stateVersion !== version) {
       return undefined;
     }
 
@@ -15,6 +18,7 @@ export const loadState = () => {
 export const saveState = (state) => {
   try {
     const serializedState = JSON.stringify(state);
+    localStorage.setItem('state-version', version);
     localStorage.setItem('state', serializedState);
   } catch (err) {
     console.error(err);
